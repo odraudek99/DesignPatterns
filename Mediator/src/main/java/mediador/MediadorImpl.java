595f7371647abcf6cdebb6e95e4aa75dadf86d5e
odraudek99 <odraudek99>
2016-05-27 10:56:59 -0500

    mvn

--- Mediator/src/main/java/mediador/MediadorImpl.java
+++ Mediator/src/main/java/mediador/MediadorImpl.java
@@ -13,48 +13,48 @@ public class MediadorImpl implements Mediador {
 	private Avion avion;
 	
 	
-	@Override
+
 	public void registerAvion(Avion avion) {
 		this.avion = avion;
 	}
 	
-	@Override
+
 	public void registerPista(Pista pista) {
 		this.pista = pista;
 	}
 	
-	@Override
+
 	public void registerServicios(ServiciosAuxilio serv) {
 		this.servicios = serv;
 	}
 	
-	@Override
+
 	public void registerSala(SalaEspera sala) {
 		this.sala = sala;
 	}
 	
-	@Override
+
 	public void updateStatePista() {
 		sala.setInformacion("Pista actualizada");
 		servicios.setInformacion("Pista actualizada");
 		avion.setInformacion("Servicios actualizados");
 	}
 	
-	@Override
+
 	public void updateStateSala() {
 		pista.setInformacion("Sala actualizada");
 		servicios.setInformacion("Sala actualizada");
 		avion.setInformacion("Servicios actualizados");
 	}
 	
-	@Override
+
 	public void updateStateServicios() {
 		pista.setInformacion("Servicios actualizados");
 		sala.setInformacion("Servicios actualizados");
 		avion.setInformacion("Servicios actualizados");
 	}
 	
-	@Override
+
 	public void updateStateAvion() {
 		pista.setInformacion("Avion actualizado");
 		sala.setInformacion("Avion actualizado");
